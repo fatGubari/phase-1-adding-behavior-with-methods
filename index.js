b0ccfdc0b34dc99c9daa7d15d1a6df3b4577517b
fatima abdullah <fatimajubary@gmail.com>
2023-02-25 12:39:07 +0300

    completed assignment

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ class Bird {
         if (this.sex === "male") {
             return `It's me! ${this.name}, the parrot!`
         } else {
-            return `${this.name}, says Squawk!!`
+            return `${this.name}, says Squawk!`
         }
     }
 }
